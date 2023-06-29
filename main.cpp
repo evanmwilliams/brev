01930408093d611610044b1a0508b6894c3dc144
Bryant Park <bryantpark04@gmail.com>
2023-06-29 00:36:14 -0400

    feat: render text as it's being typed
    - change the quit button to `

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include "TextBuffer.hpp"
 #include "PieceTable.hpp"
 
-int main(int argc, char *argv[])
+int main()
 {
+  // initialize ncurses screen
   initscr();
   raw();
   noecho();
+  scrollok(stdscr, TRUE); // Enable scrolling
 
   int ch;
   PieceTable pt;
-  while ((ch = getch()) != '\n' && ch != KEY_F(1))
+  while ((ch = getch()) != '`' && ch != KEY_F(1))
   {
     if (ch == KEY_BACKSPACE || ch == 127)
     {
@@ -19,6 +21,31 @@ int main(int argc, char *argv[])
     {
       pt.insert(std::string(1, ch));
     }
+
+    // clear screen
+    erase();
+
+    // write everything in the buffer to screen
+    int row = 0;
+    int col = 0;
+    for (char ch : pt.getText())
+    {
+      if (ch == '\n')
+      {
+        // Move to the next line
+        row++;
+        col = 0;
+        move(row, col);
+      }
+      else
+      {
+        // Print the character
+        mvprintw(row, col, "%c", ch);
+        col++;
+      }
+    }
+
+    refresh();
   }
 
   std::string buffer_contents = pt.getText();
